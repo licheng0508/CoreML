081586283aa6477362e955f9a9b38cdcdfe11137
licheng <lc900508@sina.com>
2018-05-24 11:36:35 +0800

    commit

---
+++ CoreMLTest/CoreMLTest/CoreMLViewController.swift
@@ -0,0 +1,122 @@
+//
+//  CoreMLViewController.swift
+//  CoreMLTest
+//
+//  Created by 武汉智驾 on 2018/5/21.
+//  Copyright © 2018年 licheng. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class CoreMLViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+//    var model: Inceptionv3!
+//    var model: MobileNet!
+//    var model: SqueezeNet!
+//    var model: GoogLeNetPlaces!
+//    var model: VGG16!
+
+    var model: Flowers!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        model = Flowers()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /// 相机
+    @IBAction func cameraClick(_ sender: Any) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        present(cameraPicker, animated: true)
+    }
+    
+    /// 相册
+    @IBAction func photoClick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+}
+extension CoreMLViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true)
+        textLabel.text = "Analyzing Image..."
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: imageView.frame.width, height: imageView.frame.height), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: imageView.frame.width, height: imageView.frame.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+        
+//        guard let prediction = try? model.prediction(sceneImage: pixelBuffer!) else {
+//            return
+//        }
+//
+//        textLabel.text = "I think this is a \(prediction.sceneLabel)."
+        
+
+        guard let prediction = try? model.prediction(data: pixelBuffer!) else {
+            return
+        }
+        textLabel.text = "I think this is a \(prediction.classLabel)."
+        
+//        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+//            return
+//        }
+//
+//        textLabel.text = "I think this is a \(prediction.classLabel)."
+        
+    }
+}
